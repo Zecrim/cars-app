25e70f1761d627a98d33a84d488e80b66894cb0e
User Name <zbrustkern@gmail.com>
2024-05-30 13:18:49 -0700

    built dashboard list

--- react-jwt-auth-template-main/src/components/Dashboard/Dashboard.jsx
+++ react-jwt-auth-template-main/src/components/Dashboard/Dashboard.jsx
@@ -1,13 +1,14 @@
 import { AuthedUserContext } from '../../App';
 import { useContext } from 'react';
+import GarageList from '../GarageList/GarageList'
 
-const Dashboard = ({}) => {
+const Dashboard = (props) => {
   const user = useContext(AuthedUserContext);
   return (
     <main>
       <h1>Welcome, {user.username}</h1>
       <p>
-        This is where you will be able to see all of YOUR garages
+        <GarageList garages={props.garages} userId={user._id} />
       </p>
     </main>
   );
